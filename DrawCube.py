1340699fab845b5ccb9639ecf75353569bc5d7a4
Rubén Darío Jordan Sáenz <ruben103096@gmail.com>
2019-06-24 19:02:08 -0500

    Se inician algunas funciones para el inicio de la aplicaion con presionar el boton de correr para mostrar un mensaje en pantalla

--- DrawCube.py
+++ DrawCube.py
@@ -2,3 +2,22 @@
 #Description-
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
+
+#Dimension of the edges in the cube, units in cm
+edges = .2
+
+#Definir los objetos de costruccion de aplicacion
+app = adsk.core.Application.get()
+ui  = app.userInterface
+
+#Definir la fucnion de construccion
+def buildCube():
+    ui.messageBox('Success')
+
+#Definir la funcion de inicio al correr la aplicacion
+def run(context):
+    try:
+        buildCube()
+    except:
+        if ui:
+            ui.messageBox('Failed')
